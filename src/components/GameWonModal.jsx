7ffb011de5ecdac682dc91213a4342c855217cb9
Rieona Fernandes <rxf323@student.bham.ac.uk>
2025-05-17 19:23:41 +0100

    fix: Updated the UI for the game won pop-up screen.

--- src/components/GameWonModal.jsx
+++ src/components/GameWonModal.jsx
@@ -4,8 +4,16 @@ import gameWon from "../../public/gameWon.gif";
 const GameWonModal = ({ onClose, onNewGame }) => {
   return (
     <div className="fixed inset-0 bg-stone-900/30 flex items-center justify-center p-4 z-50 backdrop-blur-sm">
-      <div className="bg-stone-50 rounded-2xl p-8 max-w-md w-full shadow-waffle-active animate-pop-in">
-        <div className="text-center">
+      {/* <div className="bg-stone-50 rounded-2xl p-8 max-w-md w-full shadow-waffle-active animate-pop-in"> */}
+      <div
+        className="text-center relative overflow-hidden space-y-2 xs:space-y-3 sm:space-y-4 bg-amber-50 p-1.5 
+      xs:p-2 sm:p-4 md:p-5 lg:p-6 rounded-3xl border-2 border-amber-300 shadow-[0_5px_15px_rgba(251,191,36,0.3)] 
+      max-w-md w-full shadow-waffle-active animate-pop-in"
+      >
+        <div className="absolute top-0 right-0 w-12 md:w-16 lg:w-20 h-12 md:h-16 lg:h-20 bg-amber-100 rounded-bl-[100%]"></div>
+        <div className="absolute -bottom-2 sm:-bottom-4 left-0 w-12 md:w-16 lg:w-20 h-12 md:h-16 lg:h-20 bg-amber-100 rounded-tr-[100%]"></div>
+
+        <div className="text-center mb-2">
           <div className="py-4 flex flex-col items-center justify-center">
             <img
               src={gameWon}
@@ -14,27 +22,29 @@ const GameWonModal = ({ onClose, onNewGame }) => {
               loading="eager"
             />
           </div>
-          <h2 className="text-2xl font-semibold text-stone-800 mb-4">
+          <h2 className="text-3xl mb-4 text-amber-800 Agbalumo-bold">
             Waffle Master!
           </h2>
-          <p className="text-stone-600 mb-6">
+          <p className="mb-6 text-amber-600 Agbalumo-regular">
             Every square in place, this batch is baked to perfection.
           </p>
 
           <div className="flex flex-col gap-4">
             <button
               onClick={onNewGame}
-              className="px-6 py-3 bg-yellow-500 text-stone-700 rounded-lg 
-                       hover:bg-yellow-500/60 transition-colors font-medium
-                       w-full focus:outline-none focus:ring-2 focus:ring-stone-400"
+              className="px-6 py-3 bg-yellow-500 text-stone-50 rounded-lg shadow-md
+                       hover:from-amber-600 hover:to-amber-700 transition-colors font-medium Agbalumo-regular
+                       w-full border border-2 border-amber-300 focus:ring-2 focus:ring-amber-300
+                     bg-gradient-to-r from-amber-500 to-amber-600"
             >
               Mix a New Batch
             </button>
             <button
               onClick={onClose}
-              className="px-6 py-3 bg-yellow-500 text-stone-700 rounded-lg 
-                       hover:bg-yellow-500/60 transition-colors font-medium
-                       w-full focus:outline-none focus:ring-2 focus:ring-stone-400"
+              className="px-6 py-3 bg-yellow-500 text-stone-50 rounded-lg Agbalumo-regular 
+                       hover:from-amber-600 hover:to-amber-700 transition-colors font-medium shadow-md
+                       w-full border border-2 border-amber-300 focus:ring-2 focus:ring-amber-300
+                     bg-gradient-to-r from-amber-500 to-amber-600"
             >
               Admire Your Waffle
             </button>
